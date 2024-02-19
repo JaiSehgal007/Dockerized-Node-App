b438c6c81a2cc27925e37348a30d31edd3f608bb
JaiSehgal007 <jaisehgal511@gmail.com>
2024-02-19 17:03:07 +0530

    Created a Dockerized Basic Node Application

---
+++ index.js
@@ -0,0 +1,12 @@
+express =require('express')
+const app=express()
+
+const PORT = process.env.PORT || 8000
+
+app.get('/',(req,res)=>{
+    return res.json({message: 'Hey this is a containerized application 🐳'})
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server started on Port ${PORT}`)
+})
